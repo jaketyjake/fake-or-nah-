9996351baa9fcd1b46f84b63d63326a21d712806
Micah Finley <mjayfinley@gmail.com>
2018-06-06 10:21:01 -0500

    randomizer working

--- randomizer.js
+++ randomizer.js
@@ -1,3 +1,24 @@
-let random = Math.floor((Math.random() * 25) + 1);
 
-console.log(random)
+
+ref = firebase.database().ref('articles/');
+
+function setupObservers() {
+  ref.on('value',function(snapshot){
+    console.log(snapshot.val())
+    let articleArray = []
+    for(key in snapshot.val()) {
+      let articleKeys = snapshot.val()[key]
+      articleArray.push(articleKeys)
+    }
+    randomArticle(articleArray)
+
+  })
+}
+
+
+function randomArticle(articleArray) {
+  let random = articleArray[Math.floor(Math.random() * articleArray.length)]
+  console.log(random)
+}
+
+setupObservers()

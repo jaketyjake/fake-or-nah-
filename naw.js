0d45dea5d20126e51db5ec58bb5f7d40a279d6cf
Micah Finley <mjayfinley@gmail.com>
2018-06-07 12:28:42 -0500

    Add files via upload

---
+++ naw.js
@@ -0,0 +1,61 @@
+// initializing variables
+var i = 0 
+let fakearticle ={}
+let nawarticle = {}
+
+//fetching articles from fake news reddit api
+fetch('https://www.reddit.com/r/TheOnion/new.json?sort=all', {
+}).then(function(response){
+  return response.json()
+}).then(function(json){
+// loop to pull specific data from api and add to local dictionary
+  json.data.children.forEach(function(fake){
+    let fakeImg = fake.data.preview.images[0].source.url
+    fakearticle.img = fakeImg
+    let fakeid = fake.data.id
+    fakearticle.id = fakeid
+    let fakeurl = fake.data.url
+    fakearticle.url = fakeurl
+    let fakesub = fake.data.subreddit
+    fakearticle.subreddit = fakesub
+    let fakethumb = fake.data.thumbnail
+    fakearticle.thumb = fakethumb
+    let faketitle = fake.data.title
+    fakearticle.title = faketitle
+//create reference to firebase database and send dictionary
+    var database = firebase.database().ref()
+    var articlesRef = database.child("articles")
+// "i" as child creates node in firebase numbered in sequence
+    articlesRef.child(i).set(fakearticle)
+    i = i+ 1
+    })
+  })
+
+//fetching articles from real news reddit api
+fetch('https://www.reddit.com/r/notTheOnion/new.json?sort=all', {
+}).then(function(response){
+  return response.json()
+}).then(function(json){
+ // loop to pull specific data from api and add to local dictionary 
+  json.data.children.forEach(function(naw){
+    let nawImg = naw.data.preview.images[0].source.url
+    nawarticle.img = nawImg
+    let nawid = naw.data.id
+    nawarticle.id = nawid
+    let nawurl = naw.data.url
+    nawarticle.url = nawurl
+    let nawsub = naw.data.subreddit
+    nawarticle.subreddit = nawsub
+    let nawthumb = naw.data.thumbnail
+    nawarticle.thumb = nawthumb
+    let nawtitle = naw.data.title
+    nawarticle.title = nawtitle
+//create reference to firebase database and send dictionary
+    var database = firebase.database().ref()
+    var articlesRef = database.child("articles")
+// "i" as child creates node in firebase numbered in sequence
+    articlesRef.child(i).set(nawarticle)
+    i = i+1
+    })
+  })
+
--- randomizer.js
+++ randomizer.js
@@ -1,24 +1,69 @@
 
 
-ref = firebase.database().ref('articles/');
+let ref = firebase.database().ref('articles');
+/*
+let articleRandom = Math.floor(Math.random() * 49)
+
+
+
+var hasGottenArticle = false
+
+var aRandoArticle = firebase.database().ref(`articles/${articleRandom}`)
+
+aRandoArticle.on('value',function(snapshot){
+
+  oneArticle = snapshot.val()
+  if(oneArticle && hasGottenArticle == false ){
+    hasGottenArticle = true
+    render_article(oneArticle)
+
+  }
+
+})
+
+function render_article(article){
+
+  var articleTemplate = `
+
+    <div>
+
+      <h5>${article.title}</h5>
+
+    </div>
+
+  `
+
+  var newArticleElement = document.createElement("div")
+  newArticleElement.innerHTML = articleTemplate
+  document.body.appendChild(newArticleElement)
+
+}
+
+*/
+
 
 function setupObservers() {
+  var articleArray = []
   ref.on('value',function(snapshot){
-    console.log(snapshot.val())
-    let articleArray = []
     for(key in snapshot.val()) {
       let articleKeys = snapshot.val()[key]
       articleArray.push(articleKeys)
     }
     randomArticle(articleArray)
-
   })
+
 }
 
 
 function randomArticle(articleArray) {
-  let random = articleArray[Math.floor(Math.random() * articleArray.length)]
+  let articleRandom = Math.floor(Math.random() * articleArray.length)
+  let random = articleArray[articleRandom]
+  console.log(articleRandom)
   console.log(random)
 }
 
-setupObservers()
+
+setTimeout(function() {
+  setupObservers()
+},
+500)

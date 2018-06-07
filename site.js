db3f63c89fbc02a59344e329c64396d6ba2faf09
Molly Wood <34974982+mollywood@users.noreply.github.com>
2018-06-06 22:46:45 -0500

    attempting to add card to html

--- site.js
+++ site.js
@@ -1,1 +1,23 @@
-let database = firebase.database().ref();
+//variables
+let database = firebase.database().ref()
+
+let newsThumb = document.getElementById('cardDiv')
+
+
+
+// populating card from database
+function populateCard (random) {
+  cardDiv.innerHTML = ""
+  random.forEach(element => {
+    let cardInfo =
+    "<div class='card'>
+      <img class='card-img-top' id='newsThumb' src='${random.thumb}'>
+      <div class='card-body'>
+        <h3 class="card-title" id="newsHeadline">${random.title}</h3>
+      </div>
+    </div>"
+  cardDiv.innerHTML += cardInfo
+  });
+}
+
+populateCard()
